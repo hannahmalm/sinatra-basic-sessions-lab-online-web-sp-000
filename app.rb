359e16d33424ab261dbb99c5981ebe9b961ba082
Hannah M <hannahemalm616@gmail.com>
2021-06-23 00:34:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+configure do 
+  enable :sessions 
+  set :session_secret, "secret"
 
 class App < Sinatra::Base
-end
+  get "/" do
+  	erb :index
+	end 
+	
+	post "/checkout" do 
+	  session["secret"] = "SessionName"
+  	@session = session
+  	erb :index
+	end 
+	
+end 
+end 
